9790709844092d939c0de109ef36842f6a676e46
Duy Đat Phạm <duydatpham@gmail.com>
2021-06-17 14:14:49 +0700

    #update ts

---
+++ src/activator.ts
@@ -0,0 +1,33 @@
+import { NativeModules } from 'react-native';
+
+const tuya = NativeModules.TuyaActivatorModule;
+
+export function openNetworkSettings() {
+  return tuya.openNetworkSettings({});
+}
+
+export type InitActivatorParams = {
+  homeId: number;
+  ssid: string;
+  password: string;
+  time: number;
+  type: 'TY_EZ' | 'TY_AP' | 'TY_QR';
+};
+
+export function initActivator(
+  params: InitActivatorParams
+): Promise<any> {
+  return tuya.initActivator(params);
+}
+
+export function stopConfig() {
+  return tuya.stopConfig();
+}
+
+export function getCurrentWifi(
+  success: (ssid: string) => void,
+  error: () => void
+) {
+  // We need the Allow While Using App location permission to use this.
+  return tuya.getCurrentWifi({}, success, error);
+}
